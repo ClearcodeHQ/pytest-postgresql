4e2748f14e40f87ce8a526ca43b873a7c26fed9e
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2018-11-16 13:53:12 +0100

    Remove pylama from CI

--- tests/test_postgresql.py
+++ tests/test_postgresql.py
@@ -2,19 +2,7 @@
 import psycopg2
 import pytest
 
-from pytest_postgresql import factories
-
-query = "CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);"
-
-pg_ctl = '/usr/lib/postgresql/{ver}/bin/pg_ctl'
-
-postgresql92 = factories.postgresql_proc(pg_ctl.format(ver='9.2'), port=None)
-postgresql93 = factories.postgresql_proc(pg_ctl.format(ver='9.3'), port=None)
-postgresql94 = factories.postgresql_proc(pg_ctl.format(ver='9.4'), port=None)
-postgresql95 = factories.postgresql_proc(pg_ctl.format(ver='9.5'), port=None)
-postgresql96 = factories.postgresql_proc(pg_ctl.format(ver='9.6'), port=None)
-postgresql10 = factories.postgresql_proc(pg_ctl.format(ver='10'), port=None)
-postgresql101 = factories.postgresql_proc(pg_ctl.format(ver='10.1'), port=None)
+QUERY = "CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);"
 
 
 @pytest.mark.parametrize('postgres', (
@@ -35,30 +23,22 @@ def test_postgresql_proc(request, postgres):
 def test_main_postgres(postgresql):
     """Check main postgresql fixture."""
     cur = postgresql.cursor()
-    cur.execute(query)
+    cur.execute(QUERY)
     postgresql.commit()
     cur.close()
-
-
-postgresql_proc2 = factories.postgresql_proc(port=9876)
-postgresql2 = factories.postgresql('postgresql_proc2')
 
 
 def test_two_postgreses(postgresql, postgresql2):
     """Check two postgresql fixtures on one test."""
     cur = postgresql.cursor()
-    cur.execute(query)
+    cur.execute(QUERY)
     postgresql.commit()
     cur.close()
 
     cur = postgresql2.cursor()
-    cur.execute(query)
+    cur.execute(QUERY)
     postgresql2.commit()
     cur.close()
-
-
-postgresql_rand_proc = factories.postgresql_proc(port=None)
-postgresql_rand = factories.postgresql('postgresql_rand_proc')
 
 
 def test_rand_postgres_port(postgresql_rand):
@@ -66,9 +46,9 @@ def test_rand_postgres_port(postgresql_rand):
     assert postgresql_rand.status == psycopg2.extensions.STATUS_READY
 
 
-@pytest.mark.parametrize('run', range(2))
+@pytest.mark.parametrize('_', range(2))
 def test_postgres_terminate_connection(
-        postgresql, run):
+        postgresql, _):
     """
     Test that connections are terminated between tests.
 
--- tests/test_version.py
+++ tests/test_version.py
@@ -4,14 +4,14 @@ import pytest
 from pytest_postgresql.executor import PostgreSQLExecutor
 
 
-@pytest.mark.parametrize('input, version', (
+@pytest.mark.parametrize('ctl_input, version', (
     ('pg_ctl (PostgreSQL) 9.6.6', '9.6'),
     ('pg_ctl (PostgreSQL) 9.5', '9.5'),
     ('pg_ctl (PostgreSQL) 9.4.1', '9.4'),
     ('pg_ctl (PostgreSQL) 10.0', '10.0'),
     ('pg_ctl (PostgreSQL) 10.1', '10.1'),
 ))
-def test_versions(input, version):
+def test_versions(ctl_input, version):
     """Check correctness of the version regexp."""
-    match = PostgreSQLExecutor.VERSION_RE.search(input)
+    match = PostgreSQLExecutor.VERSION_RE.search(ctl_input)
     assert match.groupdict()['version'] == version
