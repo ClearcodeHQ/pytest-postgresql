bac242c38bd872d69250f8cc25a623ee1304fa13
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2019-10-11 16:00:53 +0200

    fixup: test class rather than fixture

--- tests/test_noopexecutor.py
+++ tests/test_noopexecutor.py
@@ -1,4 +1,11 @@
+from pytest_postgresql.factories import NoopExecutor
 
 
-def test_nooproc_version(postgresql_proc, postgresql_nooproc):
+def test_nooproc_version(postgresql_proc):
+    postgresql_nooproc = NoopExecutor(
+        postgresql_proc.host,
+        postgresql_proc.port,
+        postgresql_proc.user,
+        postgresql_proc.options
+    )
     assert postgresql_proc == postgresql_nooproc.version
