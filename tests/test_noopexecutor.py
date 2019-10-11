8becf3ba20c72a3f543257bea58b4e26e2f30525
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2019-10-11 17:14:26 +0200

    fixup: new test

--- tests/test_noopexecutor.py
+++ tests/test_noopexecutor.py
@@ -4,7 +4,7 @@ from pytest_postgresql.factories import NoopExecutor
 
 
 def test_nooproc_version(postgresql_proc):
-    """Test the wya postgresql version is being read."""
+    """Test the way postgresql version is being read."""
     postgresql_nooproc = NoopExecutor(
         postgresql_proc.host,
         postgresql_proc.port,
@@ -12,3 +12,16 @@ def test_nooproc_version(postgresql_proc):
         postgresql_proc.options
     )
     assert postgresql_proc.version == postgresql_nooproc.version
+
+
+def test_nooproc_cached_version(postgresql_proc):
+    """Test that the version is being cached."""
+    postgresql_nooproc = NoopExecutor(
+        postgresql_proc.host,
+        postgresql_proc.port,
+        postgresql_proc.user,
+        postgresql_proc.options
+    )
+    v = postgresql_nooproc.version
+    with postgresql_proc.stopped():
+        assert v == postgresql_nooproc.version
