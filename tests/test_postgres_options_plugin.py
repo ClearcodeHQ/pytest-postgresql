29752b21c8d3560416462757cf0f852b53cd7eeb
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2023-03-31 15:50:45 +0200

    Move project's dependency management to pipenv - closes #701

--- tests/test_postgres_options_plugin.py
+++ tests/test_postgres_options_plugin.py
@@ -1,18 +1,32 @@
 """Test behavior of postgres_options passed in different ways."""
+from pathlib import Path
 
+import pytest
 from pytest import Pytester
+import pytest_postgresql
 
 
-def test_postgres_options_config_in_cli(pytester: Pytester) -> None:
+@pytest.fixture
+def pointed_pytester(pytester: Pytester) -> Pytester:
+    pytest_postgresql_path = Path(pytest_postgresql.__file__)
+    root_path = pytest_postgresql_path.parent.parent
+    pytester.syspathinsert(root_path)
+    pytester.makeconftest("from pytest_postgresql.plugin import *\n")
+    return pytester
+
+
+def test_postgres_options_config_in_cli(pointed_pytester: Pytester) -> None:
     """Check that command line arguments are honored."""
-    pytester.copy_example("test_postgres_options.py")
-    ret = pytester.runpytest("--postgresql-postgres-options", "-N 11", "test_postgres_options.py")
+    pointed_pytester.copy_example("test_postgres_options.py")
+    ret = pointed_pytester.runpytest(
+        "--postgresql-postgres-options", "-N 11", "test_postgres_options.py"
+    )
     ret.assert_outcomes(passed=1)
 
 
-def test_postgres_options_config_in_ini(pytester: Pytester) -> None:
+def test_postgres_options_config_in_ini(pointed_pytester: Pytester) -> None:
     """Check that pytest.ini arguments are honored."""
-    pytester.copy_example("test_postgres_options.py")
-    pytester.makefile(".ini", pytest="[pytest]\npostgresql_postgres_options = -N 11\n")
-    ret = pytester.runpytest("test_postgres_options.py")
+    pointed_pytester.copy_example("test_postgres_options.py")
+    pointed_pytester.makefile(".ini", pytest="[pytest]\npostgresql_postgres_options = -N 11\n")
+    ret = pointed_pytester.runpytest("test_postgres_options.py")
     ret.assert_outcomes(passed=1)
