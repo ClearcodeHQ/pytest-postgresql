ee21384495897a02bea082060f5d2f784beeff60
Jared Keil <jaredrkeil@gmail.com>
2023-04-16 12:35:48 -0700

    Use a TypedDict for plugin options config - closes #706

--- pytest_postgresql/config.py
+++ pytest_postgresql/config.py
@@ -1,25 +1,41 @@
+from typing import Optional, TypedDict
+
 from pytest import FixtureRequest
 
 
-def get_config(request: FixtureRequest) -> dict:
+class PostgresqlConfigDict(TypedDict):
+    exec: str
+    host: str
+    port: Optional[str | int]
+    user: str
+    password: Optional[str]
+    options: str
+    startparams: str
+    logsprefix: str
+    unixsocketdir: str
+    dbname: str
+    load: Optional[list[str]]
+    postgres_options: str
+
+
+def get_config(request: FixtureRequest) -> PostgresqlConfigDict:
     """Return a dictionary with config options."""
-    config = {}
-    options = [
-        "exec",
-        "host",
-        "port",
-        "user",
-        "password",
-        "options",
-        "startparams",
-        "logsprefix",
-        "unixsocketdir",
-        "dbname",
-        "load",
-        "postgres_options",
-    ]
-    for option in options:
-        option_name = "postgresql_" + option
-        conf = request.config.getoption(option_name) or request.config.getini(option_name)
-        config[option] = conf
-    return config
+
+    def get_postgresql_option(option: str):
+        name = "postgresql_" + option
+        return request.config.getoption(name) or request.config.getini(name)
+
+    return PostgresqlConfigDict(
+        exec=get_postgresql_option("exec"),
+        host=get_postgresql_option("host"),
+        port=get_postgresql_option("port"),
+        user=get_postgresql_option("user"),
+        password=get_postgresql_option("password"),
+        options=get_postgresql_option("options"),
+        startparams=get_postgresql_option("startparams"),
+        logsprefix=get_postgresql_option("logsprefix"),
+        unixsocketdir=get_postgresql_option("unixsocketdir"),
+        dbname=get_postgresql_option("dbname"),
+        load=get_postgresql_option("load"),
+        postgres_options=get_postgresql_option("postgres_options")
+    )
