e96649ac0107e75bf202dc3a970ab255909ca1e3
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2024-02-15 09:09:13 +0100

    Change really old package's name to the new one in License

--- pytest_postgresql/factories/process.py
+++ pytest_postgresql/factories/process.py
@@ -3,18 +3,18 @@
 
 # This file is part of pytest-postgresql.
 
-# pytest-dbfixtures is free software: you can redistribute it and/or modify
+# pytest-postgresql is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# pytest-dbfixtures is distributed in the hope that it will be useful,
+# pytest-postgresql is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 
 # You should have received a copy of the GNU Lesser General Public License
-# along with pytest-dbfixtures.  If not, see <http://www.gnu.org/licenses/>.
+# along with pytest-postgresql.  If not, see <http://www.gnu.org/licenses/>.
 """Fixture factory for postgresql process."""
 import os.path
 import platform
